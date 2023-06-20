2c9de03fd76e7a8c488a88d27f7bee60c3ca67dd
rickyrev <132206679+rickyrev@users.noreply.github.com>
2023-06-20 23:39:28 +0700

    Update klse.py

--- klse.py
+++ klse.py
@@ -12,3 +12,14 @@ print("Selamat datang di Program Kasir Swalayan!")
 nama = input("Silakan masukkan nama Anda: ")
 items = []
 prices = []
+
+while True:
+    print("Berikut adalah daftar barang dan harga:")
+    for k, v in daftar_barang.items():
+        print(f"{k}: Rp {v:,}")
+    item = input("Masukkan nama barang yang ingin Anda beli (atau 'selesai' untuk menyelesaikan order): ")
+    if item == 'selesai':
+        break
+    if item not in daftar_barang:
+        print("Barang yang Anda masukkan tidak tersedia.")
+        continue
