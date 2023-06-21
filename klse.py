8ea1ac9793533e475967d8ebbb1587640e4b0253
shalsachalista02 <137140181+shalsachalista02@users.noreply.github.com>
2023-06-21 08:41:03 +0700

    Update klse.py
    1. Terminal digunakan sebagai media untuk menampilkan output dari program.
    
    2. Fungsi print_receipt mencetak sebuah struktur nota pembelian ke terminal.
    
    3. Output yang dihasilkan oleh fungsi print_receipt terdiri dari beberapa baris teks yang mencakup informasi tentang setiap item yang dibeli, harga, dan total pembelian.
    
    4. Setiap baris teks yang dicetak menggunakan format string dengan menggunakan metode format() untuk memformat data yang akan ditampilkan.
    
    5. Setelah selesai memproses semua item, fungsi print_receipt mencetak total pembelian.
    
    6. Output tersebut akan ditampilkan di terminal ketika fungsi print_receipt dipanggil dengan argumen items dan prices.
    
    Dengan memanggil print_receipt(items, prices) diakhir kode, kita dapat melihat output nota pembelian di terminal. Output tersebut akan mencakup daftar item yang dibeli beserta harga per item dan total pembelian

--- klse.py
+++ klse.py
@@ -23,3 +23,14 @@ while True:
     if item not in daftar_barang:
         print("Barang yang Anda masukkan tidak tersedia.")
         continue
+
+def print_receipt(items, prices):
+    print(f"\n\nNota Pembelian\n-----------------------")
+    total = 0
+    for i in range(len(items)):
+        print("{:<20} {:>2} x Rp {:>6.2f} = Rp {:>8.2f}".format(items[i], prices[i] // daftar_barang[items[i]], daftar_barang[items[i]], prices[i]))
+        total += prices[i]
+    print("-----------------------")
+    print("{:<20} Rp {:>8.2f}".format("Total:", total))
+
+print_receipt(items, prices)
